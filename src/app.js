02acdc11b5b8b683e55da52f6d76ac6b606b59f9
andradegali1503-droid <andradegali1503@gmail.com>
2026-02-23 21:34:30 +0000

    finish corrige error en oraciones

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    const who =['the dog', 'My grandma','The mailman','My bird'];
+    const action=['ate', 'ped on ', 'crushed','broke'];
+    const what=['my homewrok','myphone','the car'];
+    const when=['before the glass', 'whe I was sleeping', 'while I was Exercising','durin my lunch','while I was praying'];
+
+    let randomWho=who[Math.floor(Math.random()*who.length)];
+    let randomAction=action[Math.floor(Math.random()*action.length)];
+    let randomWaht=what[Math.floor(Math.random()*what.length)];
+    let randomWhen=when[Math.floor(Math.random()*when.length)];
+
+
+    let excuse= randomWho+" "+randomAction+" "+randomWaht+" "+randomWhen;
+
+
+    document.getElementById("excuse").innerHTML= excuse;
+
+
+
 };
